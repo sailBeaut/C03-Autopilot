3edb64d771f89a0d3159b9b4753ba5103ada16d8
LennartH21 <lennart.hubbers@gmail.com>
2025-02-27 17:12:09 +0100

    cut data

--- check_data.py
+++ check_data.py
@@ -8,4 +8,4 @@ with h5py.File(file_path, "r") as f:
     def print_structure(name, obj):
         print(name, "->", "Group" if isinstance(obj, h5py.Group) else "Dataset")
     f.visititems(print_structure)
-    print(f["run9/aircraft/DeltaElev"][()])
+    h = f["run1/aircraft/IservoAil"][()]
