bc12876a97bb2fa3d678bcbb40320245c37b3aa1
LennartH21 <lennart.hubbers@gmail.com>
2025-02-27 17:01:56 +0100

    fixed runs

--- get_data.py
+++ get_data.py
@@ -27,7 +27,7 @@ import h5py
 def extract_relevant_data(input_file, output_file, runs=12):
     """Extracts DeltaAil and DeltaElev data from multiple runs and saves to a new HDF5 file."""
     with h5py.File(input_file, 'r') as src, h5py.File(output_file, 'w') as dest:
-        for i in range(1, runs + 1):  # Assuming runs are named run1, run2, ..., run12
+        for i in [1,3,4,5,6,7,8,9,10,11,12,13]:  # Assuming runs are named run1, run2, ..., run12
             for dataset in ['DeltaAil', 'DeltaElev', 'IservoAil', 'IservoElev', 'DeltaDrumElev', 'DeltaDrumAil']:
                 run_path = f'run{i}/aircraft/{dataset}'
                 
