3cfb913a9d5bb245a3793027e63964197d84866f
erenceylan1 <ceylaneren@gmail.com>
2025-03-06 14:34:37 +0100

    test

--- main.py
+++ main.py
@@ -17,10 +17,24 @@ kg = 1.0  # gain SET PARAMETER
 Ie = 1.0  # moment of inertia TUNING PARAMETER
 
 # State equation
-A = sp.Matrix([[-(c1/Ie), -(k1/Ie)], [1, 0]])
-B = sp.Matrix([[kg/Ie], [0]])
-x = sp.Matrix([[DeltaAil_dot], [DeltaAil]])
-u = sp.Matrix([[IservoAil]])
+A = sp.MatrixSymbol('A', 2, 2)  # 2x2 symbolic matrix
+B = sp.MatrixSymbol('B', 2, 1)  # 2x1 symbolic matrix
+x = sp.MatrixSymbol('x', 2, 1)  # 2x1 symbolic vector
+u = sp.MatrixSymbol('u', 1, 1)  # Scalar input as a 1x1 matrix
 
-def state_equation():
-    xdot = A @ x + B @ u
+
+
+A_val = sp.Matrix([[-(c1/Ie), -(k1/Ie)], [1, 0]])
+B_val = sp.Matrix([[kg/Ie], [0]])
+x_val = sp.Matrix([[DeltaAil_dot], [DeltaAil]])
+u_val = sp.Matrix([[IservoAil]])
+
+
+xdot = A @ x + B @ u
+
+xdot_evaluated = xdot.subs({A: A_val, B: B_val, x: x_val, u: u_val})
+
+# Simplify and compute the result
+xdot_result = xdot_evaluated.doit()
+
+print(xdot_result)
