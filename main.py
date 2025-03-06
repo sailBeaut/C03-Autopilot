43ba95350d5b91bfffb81da0e0199884f77a3f37
LennartH21 <lennart.hubbers@gmail.com>
2025-03-06 14:45:54 +0100

    for loop

--- main.py
+++ main.py
@@ -5,11 +5,6 @@ from check_data import dat_array, print_struc
 DeltaAil = dat_array("run1/aircraft/DeltaAil")
 IservoAil = dat_array("run1/aircraft/IservoAil")
 
-# Define symbolic variables
-DeltaAil = sp.Symbol("DeltaAil")
-DeltaAil_dot = sp.Symbol("DeltaAil_dot")
-IservoAil = sp.Symbol("IservoAil")
-
 # Parametric constants
 c1 = 1.0  # damper constant TUNING PARAMETER
 k1 = 1.0  # spring constant TUNING PARAMETER
@@ -23,18 +18,22 @@ x = sp.MatrixSymbol('x', 2, 1)  # 2x1 symbolic vector
 u = sp.MatrixSymbol('u', 1, 1)  # Scalar input as a 1x1 matrix
 
 
-
 A_val = sp.Matrix([[-(c1/Ie), -(k1/Ie)], [1, 0]])
 B_val = sp.Matrix([[kg/Ie], [0]])
-x_val = sp.Matrix([[DeltaAil_dot], [DeltaAil]])
-u_val = sp.Matrix([[IservoAil]])
+x_val = sp.Matrix([[0], [DeltaAil[0]]])
 
 
 xdot = A @ x + B @ u
 
-xdot_evaluated = xdot.subs({A: A_val, B: B_val, x: x_val, u: u_val})
+for i in range(len(DeltaAil)):
+	u_val = sp.Matrix([[IservoAil[i]]])
+	if i !=0:
+		x_val = sp.Matrix([[xdot_result[0]], [DeltaAil[i]]])
+	xdot_evaluated = xdot.subs({A: A_val, B: B_val, x: x_val, u: u_val})
+	xdot_result = xdot_evaluated.doit()
+print(xdot_result)
 
 # Simplify and compute the result
-xdot_result = xdot_evaluated.doit()
 
-print(xdot_result)
+
+
