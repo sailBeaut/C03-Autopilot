cf86600c5d9cb96a948e27ae0f853482ab0348ad
LennartH21 <lennart.hubbers@gmail.com>
2025-03-11 11:44:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,29 +9,32 @@ DeltaAil = dat_array("run8/aircraft/DeltaAil")
 IservoAil = dat_array("run8/aircraft/IservoAil")
 
 # Parametric constants
-c1 = 2.95  # damper constant TUNING PARAMETER
-k1 = 9.9  # spring constant TUNING PARAMETER
-kg = 0.45  # gain SET PARAMETER
-Ie = 0.4  # moment of inertia TUNING PARAMETER
+c1 = 3  # damper constant TUNING PARAMETER
+k1 = 7  # spring constant TUNING PARAMETER
+kg = 0.4  # gain SET PARAMETER
+Ie = 0.45  # moment of inertia TUNING PARAMETER
+c2 = 0.001
 
 # Define symbolic matrices
 A = sp.Matrix([[-(c1/Ie), -(k1/Ie)], 
                [1, 0]])
 B = sp.Matrix([[-kg/Ie], 
                [0]])
-
+C = sp.Matrix([[c2/Ie], 
+               [0]])
 
 # Initial conditions
-x = sp.Matrix([[0], [0.013]])  # Initial state (angle and velocity)
+x = sp.Matrix([[0], [0.007]])  # Initial state (angle and velocity)
 xlist = []
 
 # Time integration loop
 dt = 0.001  # Time step
 for i in range(7001):
     u = sp.Matrix([[IservoAil[i]]])  # Control input
+    v = sp.Matrix([86])
     
     # Compute xdot = A * x + B * u
-    xdot = A @ x + B @ u
+    xdot = A @ x + B @ u + C @ v
     #print(xdot)
     x = x + xdot * dt  # Euler integration step
 
