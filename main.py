b95ebf711724466d98adc2850a9603ae5f85c4e9
sanderfdm <sander.fadam@gmail.com>
2025-03-11 09:04:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Load data
-DeltaAil = dat_array("run1/aircraft/DeltaAil")
-IservoAil = dat_array("run1/aircraft/IservoAil")
+DeltaAil = dat_array("run8/aircraft/DeltaAil")
+IservoAil = dat_array("run8/aircraft/IservoAil")
 
 # Parametric constants
-c1 = 3.0  # damper constant TUNING PARAMETER
-k1 = 9.7  # spring constant TUNING PARAMETER
+c1 = 2.95  # damper constant TUNING PARAMETER
+k1 = 9.9  # spring constant TUNING PARAMETER
 kg = -0.45  # gain SET PARAMETER
-Ie = 0.31  # moment of inertia TUNING PARAMETER
+Ie = 0.4  # moment of inertia TUNING PARAMETER
 
 # Define symbolic matrices
 A = sp.Matrix([[-(c1/Ie), -(k1/Ie)], 
@@ -37,8 +37,8 @@ for i in range(7001):
     # Store angle (first element of x)
     xlist.append(x[1])
 
-    if i % 1000 == 0:
-        print(f"Step {i}: x = {x}")
+    #if i % 1000 == 0:
+        #print(f"Step {i}: x = {x}")
 error = sum((xlist-DeltaAil)**2)
 # Plot results
 plt.plot(np.linspace(0, 7000, 7001), xlist, color="r", label="Computed")
