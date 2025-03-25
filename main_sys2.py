e0901eb60ef38b73d717af3e2252b68048e6f663
Boef23 <basvanstrien@gmail.com>
2025-03-25 12:03:19 +0100

    xas

--- main_sys2.py
+++ main_sys2.py
@@ -6,12 +6,12 @@ import matplotlib.pyplot as plt
 from Functions_for_sys2 import model2_aileron, accuracy_plot
 
 #Parameters
-k1_numvalue = 400000
-k2_numvalue = 9 
+k1_numvalue = 390000
+k2_numvalue = 11.5 
 c1_numvalue = 350
-c2_numvalue = 3
+c2_numvalue = 6
 k_g = 0.22
-a_velo = 0.0000004
+a_velo = 0 #0.00000015
 
 #On Or Off
 extragraphs = False
@@ -30,7 +30,7 @@ run11_acc1, run11_acc2 = model2_aileron(11, k_g, k1_numvalue, k2_numvalue, c1_nu
 accuracy_DOF1 = [run1_acc1, run3_acc1, run8_acc1, run9_acc1, run10_acc1, run11_acc1]
 accuracy_DOF2 = [run1_acc2, run3_acc2, run8_acc2, run9_acc2, run10_acc2, run11_acc2]
 
-# Plot accuracy
+# # # Plot accuracy
 
 accuracy_plot(accuracy_DOF1, accuracy_DOF2)
 
