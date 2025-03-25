e851fda6897a9e2a0a26646a1ac35f9cd0f8bbb1
LennartH21 <lennart.hubbers@gmail.com>
2025-03-25 10:50:42 +0100

    i hope this worked

--- get_data.py
+++ get_data.py
@@ -1,10 +1,11 @@
 import h5py
+import numpy as np
 
 def extract_relevant_data(input_file, output_file, runs=12):
 	"""Extracts DeltaAil and DeltaElev data from multiple runs and saves to a new HDF5 file."""
 	with h5py.File(input_file, 'r') as src, h5py.File(output_file, 'w') as dest:
 		for i in [1,3,4,5,6,7,8,9,10,11,12,13]:  # Assuming runs are named run1, run2, ..., run12
-			for dataset in ['DeltaAil', 'DeltaElev', 'IservoAil', 'IservoElev', 'DeltaDrumElev', 'DeltaDrumAil', 'DynPress']:
+			for dataset in ['DeltaElev', 'IservoAil', 'IservoElev', 'DeltaDrumElev', 'DeltaDrumAil', 'DynPress']:
 				run_path = f'run{i}/aircraft/{dataset}'
 				
 				if run_path in src:
@@ -12,7 +13,9 @@ def extract_relevant_data(input_file, output_file, runs=12):
 					dest.create_dataset(f'run{i}/aircraft/{dataset}', data=data)
 				else:
 					print(f"Warning: {run_path} not found in source file.")
-			
+			if run_path in src:
+				data = np.array(src[run_path][4000:11001])-src[run_path][4000:11001]  # Extract subset of data
+				dest.create_dataset(f'run{i}/aircraft/DeltaAil', data=data)			
 			servo_path = f'run{i}/servo/delta_e_t'
 			if servo_path in src:
 				data = src[servo_path][4000:11001]  # Extract subset of data
