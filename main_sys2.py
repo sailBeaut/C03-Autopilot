c463a565bc91eabdee310830484e8bc0b34d5916
Boef23 <basvanstrien@gmail.com>
2025-03-25 10:51:34 +0100

    jjh

--- main_sys2.py
+++ main_sys2.py
@@ -18,6 +18,7 @@ extragraphs = False
 showmainplots = False
 printeigenvalues = False
 
+#Run the model
 run1_acc1, run1_acc2 =   model2_aileron(1, k_g, k1_numvalue, k2_numvalue, c1_numvalue, c2_numvalue, a_velo, extragraphs, showmainplots, printeigenvalues)
 run3_acc1, run3_acc2 =   model2_aileron(3, k_g, k1_numvalue, k2_numvalue, c1_numvalue, c2_numvalue, a_velo, extragraphs, showmainplots, printeigenvalues)
 run8_acc1, run8_acc2 =   model2_aileron(8, k_g, k1_numvalue, k2_numvalue, c1_numvalue, c2_numvalue, a_velo, extragraphs, showmainplots, printeigenvalues)
@@ -25,6 +26,7 @@ run9_acc1, run9_acc2 =   model2_aileron(9, k_g, k1_numvalue, k2_numvalue, c1_num
 run10_acc1, run10_acc2 = model2_aileron(10, k_g, k1_numvalue, k2_numvalue, c1_numvalue, c2_numvalue, a_velo, extragraphs, showmainplots, printeigenvalues)
 run11_acc1, run11_acc2 = model2_aileron(11, k_g, k1_numvalue, k2_numvalue, c1_numvalue, c2_numvalue, a_velo, extragraphs, showmainplots, printeigenvalues)
 
+# Accuracy
 accuracy_DOF1 = [run1_acc1, run3_acc1, run8_acc1, run9_acc1, run10_acc1, run11_acc1]
 accuracy_DOF2 = [run1_acc2, run3_acc2, run8_acc2, run9_acc2, run10_acc2, run11_acc2]
 
