c08bc70d40c908f8441e6e5dd8559ef1d4b57ee9
LennartH21 <lennart.hubbers@gmail.com>
2025-04-24 14:39:40 +0200

    comments

--- main.py
+++ main.py
@@ -1,40 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from check_data import dat_array
+from check_data import dat_array as load_data
 
-# Load data
-run_nr = 1
-DeltaAil = dat_array("run" + str(run_nr) + "/aircraft/DeltaAil")
-IservoAil = dat_array("run" + str(run_nr) + "/aircraft/IservoAil")
+# Load the data for a specific run
+run_nr = 11
+DeltaAil = load_data("run" + str(run_nr) + "/aircraft/DeltaAil")
+IservoAil = load_data("run" + str(run_nr) + "/aircraft/IservoAil")
 
 # Tuning Parameters 
-c1 = 1.37  # Damper constant (TUNING PARAMETER)
-k1 = 4.09   # Spring constant (TUNING PARAMETER)
+c1 = 1.37  # Damper constant 
+k1 = 4.09   # Spring constant 
 
 # Set Parameters
-kg = 0.22      # Gain (SET PARAMETER)
-Ie = 0.0451    # Average Moment of inertia (SET PARAMETER)
+kg = 0.22 # Gain 
+Ie = 0.0451 # Moment of inertia 
 
 # System matrices
-A = np.array([[-(c1/Ie), -(k1/Ie)], [1, 0]])
-B = np.array([[-kg/Ie], [0]])
+A = np.array([[-(c1/Ie), -(k1/Ie)], [1, 0]]) 
+B = np.array([[-kg/Ie], [0]]) 
 
-# Initial state [angle, velocity]
-x = np.array([[0], [DeltaAil[0]]])
-xlist = []
+x = np.array([[0], [DeltaAil[0]]]) # Initial state [angle, velocity]
+xlist = [] # List to store state vectors
 
 # Time integration loop
-dt = 0.001  # Time step
+dt = 0.001  # Time step (1ms)
 for i in range(7001):
-    u = np.array([[IservoAil[i]]])  # Control input
-    v = np.array([[(dat_array("run" + str(run_nr) + "/aircraft/DynPress"))[i]]])  # External force
-    
-    # Compute xdot = A * x + B * u + C * v
-    xdot = A @ x + B @ u 
+    u = np.array([[IservoAil[i]]]) # Control current input    
+
+    xdot = A @ x + B @ u # Compute the derivative from system matrices and inputs
     x = x + xdot * dt  # Euler integration step
 
-    # Store angle (first element of x)
-    xlist.append(x[1, 0])
+    xlist.append(x[1, 0]) # Store control surface deflection
 
 # Convert xlist to NumPy array
 xlist = np.array(xlist)
@@ -42,14 +38,14 @@ xlist = np.array(xlist)
 # Compute absolute error
 absolute_error = np.abs(xlist - DeltaAil)
 
-# Compute accuracy as percentage
+# Compute relative error and accuracy as percentages
 error_norm = np.linalg.norm(absolute_error) / np.linalg.norm(DeltaAil)
 accuracy = (1 - error_norm) * 100
 
 # Print accuracy
 print(f"Model Accuracy: {accuracy:.2f}%")
 
-# Plot computed vs actual Delta Ail
+# Plot computed vs actual Delta Ail over time
 time_steps = np.linspace(0, 7000, 7001)
 plt.figure(figsize=(10, 5))
 plt.plot(time_steps, xlist, color="r", label="Computed")
@@ -60,11 +56,11 @@ plt.legend()
 plt.title("Computed vs. Actual Delta Ail")
 plt.show()
 
-# # Plot absolute error over time
-# plt.figure(figsize=(10, 5))
-# plt.plot(time_steps, absolute_error, color="g", label="Absolute Error")
-# plt.xlabel("Time Steps")
-# plt.ylabel("Absolute Error")
-# plt.legend()
-# plt.title("Absolute Error Over Time")
-# plt.show()
+# Plot absolute error over time
+plt.figure(figsize=(10, 5))
+plt.plot(time_steps, absolute_error, color="g", label="Absolute Error")
+plt.xlabel("Time Steps")
+plt.ylabel("Absolute Error")
+plt.legend()
+plt.title("Absolute Error Over Time")
+plt.show()
