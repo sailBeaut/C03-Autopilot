16b3e7b6afde95c4006aac006a9ba01269782f24
Boef23 <basvanstrien@gmail.com>
2025-04-29 12:00:25 +0200

    daw

--- main_sys2.py
+++ main_sys2.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from Functions_for_sys2 import model2, accuracy_plot_ail, accuracy_plot_elev
 
 #Resolution
-resolution = 1
+resolution = 0.1
 #Parameters Aileron
 divfactor_ail = 2
 k1_numvalue_ail = 400000
