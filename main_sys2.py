322762890a65355c3b29630f20375c197340b61a
toskap <toskap@protonmail.com>
2025-04-29 10:11:54 +0200

    toska

--- main_sys2.py
+++ main_sys2.py
@@ -6,28 +6,28 @@ import matplotlib.pyplot as plt
 from Functions_for_sys2 import model2, accuracy_plot_ail, accuracy_plot_elev
 
 #Parameters Aileron
-divfactor = 2
+divfactor_ail = 2
 k1_numvalue_ail = 400000
 k2_numvalue_ail = 10.8
 c1_numvalue_ail = 350
 c2_numvalue_ail = 4.5
-k_g = 0.22
+k_g_ail = 0.22
 a_velo_ail = 0.0000001
-flip = -1
+flip_ail = -1
 
 #Parameters Elevator
-divfactor = 2
+divfactor_elev = 2
 k1_numvalue_elev = 400000
 k2_numvalue_elev = 10.8
 c1_numvalue_elev = 350
 c2_numvalue_elev = 4.5
-k_g = 0.22
+k_g_elev = 0.22
 a_velo_elev = 0.0000001
-flip = 1
+flip_elev = 1
 
 #On Or Off
-aileron = True
-elevator = False
+aileron = False
+elevator = True
 extragraphs = False
 showmainplots = True
 printeigenvalues = False
@@ -45,12 +45,12 @@ for run in range(1, 14):
         continue
     else:
         if run in (1, 3, 8, 9, 10, 11) and aileron == True:
-            acc_run_DOF1, acc_run_DOF2 = model2(run, flip, divfactor, k_g, k1_numvalue_ail, k2_numvalue_ail, c1_numvalue_ail, c2_numvalue_ail, a_velo_ail, extragraphs, showmainplots, printeigenvalues)
+            acc_run_DOF1, acc_run_DOF2 = model2(run, flip_ail, divfactor_ail, k_g_elev, k1_numvalue_ail, k2_numvalue_ail, c1_numvalue_ail, c2_numvalue_ail, a_velo_ail, extragraphs, showmainplots, printeigenvalues)
             # Calculate accuracy for aileron
             accuracy_DOF1_ail.append(acc_run_DOF1)
             accuracy_DOF2_ail.append(acc_run_DOF2)
         elif run in (4, 5, 6, 7, 12, 13) and elevator == True:
-            acc_run_DOF1, acc_run_DOF2 = model2(run, flip, divfactor, k_g, k1_numvalue_elev, k2_numvalue_elev, c1_numvalue_elev, c2_numvalue_elev, a_velo_elev, extragraphs, showmainplots, printeigenvalues)
+            acc_run_DOF1, acc_run_DOF2 = model2(run, flip_elev, divfactor_elev, k_g_elev, k1_numvalue_elev, k2_numvalue_elev, c1_numvalue_elev, c2_numvalue_elev, a_velo_elev, extragraphs, showmainplots, printeigenvalues)
             # Calculate accuracy for elevator
             accuracy_DOF1_elev.append(acc_run_DOF1)
             accuracy_DOF2_elev.append(acc_run_DOF2)
