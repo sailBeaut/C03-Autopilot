16feca3c5605055e71e301f40012cb086366f401
Boef23 <basvanstrien@gmail.com>
2025-04-29 11:59:14 +0200

    dad

--- main_sys2.py
+++ main_sys2.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from Functions_for_sys2 import model2, accuracy_plot_ail, accuracy_plot_elev
 
 #Resolution
-resolution = 5
+resolution = 1
 #Parameters Aileron
 divfactor_ail = 2
 k1_numvalue_ail = 400000
@@ -18,7 +18,7 @@ a_velo_ail = 0.0000001
 flip_ail = -1
 
 #Parameters Elevator
-divfactor_elev = 4
+divfactor_elev = 1
 k1_numvalue_elev = 500000
 k2_numvalue_elev = 10.8
 c1_numvalue_elev = 200
