9877b1dd1a744d18ff635e83d18aa6a73c9fe858
Boef23 <basvanstrien@gmail.com>
2025-04-29 10:31:19 +0200

    adw

--- main_sys2.py
+++ main_sys2.py
@@ -5,6 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from Functions_for_sys2 import model2, accuracy_plot_ail, accuracy_plot_elev
 
+#Resolution
+resolution = 10
 #Parameters Aileron
 divfactor_ail = 2
 k1_numvalue_ail = 400000
@@ -45,12 +47,12 @@ for run in range(1, 14):
         continue
     else:
         if run in (1, 3, 8, 9, 10, 11) and aileron == True:
-            acc_run_DOF1, acc_run_DOF2 = model2(run, flip_ail, divfactor_ail, k_g_elev, k1_numvalue_ail, k2_numvalue_ail, c1_numvalue_ail, c2_numvalue_ail, a_velo_ail, extragraphs, showmainplots, printeigenvalues)
+            acc_run_DOF1, acc_run_DOF2 = model2(run, resolution, flip_ail, divfactor_ail, k_g_elev, k1_numvalue_ail, k2_numvalue_ail, c1_numvalue_ail, c2_numvalue_ail, a_velo_ail, extragraphs, showmainplots, printeigenvalues)
             # Calculate accuracy for aileron
             accuracy_DOF1_ail.append(acc_run_DOF1)
             accuracy_DOF2_ail.append(acc_run_DOF2)
         elif run in (4, 5, 6, 7, 12, 13) and elevator == True:
-            acc_run_DOF1, acc_run_DOF2 = model2(run, flip_elev, divfactor_elev, k_g_elev, k1_numvalue_elev, k2_numvalue_elev, c1_numvalue_elev, c2_numvalue_elev, a_velo_elev, extragraphs, showmainplots, printeigenvalues)
+            acc_run_DOF1, acc_run_DOF2 = model2(run, resolution, flip_elev, divfactor_elev, k_g_elev, k1_numvalue_elev, k2_numvalue_elev, c1_numvalue_elev, c2_numvalue_elev, a_velo_elev, extragraphs, showmainplots, printeigenvalues)
             # Calculate accuracy for elevator
             accuracy_DOF1_elev.append(acc_run_DOF1)
             accuracy_DOF2_elev.append(acc_run_DOF2)
