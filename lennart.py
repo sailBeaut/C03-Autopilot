00c67bacce2460b503f964c86bdba7630f43d31d
LennartH21 <lennart.hubbers@gmail.com>
2025-05-06 09:48:33 +0200

    yahoo

--- lennart.py
+++ lennart.py
@@ -4,7 +4,7 @@ import numpy as np
 def extract_relevant_data(input_file, output_file, runs=12):
 	"""Extracts DeltaAil and DeltaElev data from multiple runs and saves to a new HDF5 file."""
 	with h5py.File(input_file, 'r') as src, h5py.File(output_file, 'w') as dest:
-		for i in [1,3,4,5,6,7,8,9,10,11,12,13]:  # Assuming runs are named run1, run2, ..., run12
+		for i in [1]:  # Assuming runs are named run1, run2, ..., run12
 			for dataset in ['IservoAil', 'IservoElev', 'DeltaDrumElev', 'DeltaDrumAil', 'DynPress']:
 				run_path = f'run{i}/aircraft/{dataset}'
 
@@ -27,6 +27,6 @@ def extract_relevant_data(input_file, output_file, runs=12):
 				print(f"Warning: {servo_path} not found in source file.")
 
 # Example usage
-input_hdf5 = '/Users/lennarthubbers/Desktop/processed-20250217_151129.hdf5'  # Replace with your actual input file
-output_hdf5 = 'filtered_data3.hdf5'  # Replace with your desired output file
+input_hdf5 = 'new_data.hdf5'  # Replace with your actual input file
+output_hdf5 = 'split_new_data.hdf5'  # Replace with your desired output file
 extract_relevant_data(input_hdf5, output_hdf5)
