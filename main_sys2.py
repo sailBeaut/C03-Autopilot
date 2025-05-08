885c5838389ca7a66809001b9b6d782c51823f13
Boef23 <basvanstrien@gmail.com>
2025-05-08 16:32:55 +0200

    dd

--- main_sys2.py
+++ main_sys2.py
@@ -24,16 +24,16 @@ k2_numvalue_elev = 22.5
 c1_numvalue_elev = 50
 c2_numvalue_elev = 4.5
 k_g_elev = 0.22
-a_velo_elev = 5e-7
+a_velo_elev = 4e-7
 flip_elev = 1
 
 #On Or Off
 ground = False
 aileron = True
-elevator = False
+elevator = True
 array = False
 extragraphs = False
-showmainplots = True
+showmainplots = False
 printeigenvalues = False
 
 #Define Accuracy lists
