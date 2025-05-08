d8d37c16d55445968ac0b7b0ac91f797fbd93e8c
LennartH21 <lennart.hubbers@gmail.com>
2025-05-08 16:27:11 +0200

    new data

--- check_data.py
+++ check_data.py
@@ -2,7 +2,7 @@ import h5py
 
 # Open the HDF5 file in read mode
 #file_path = "/Users/lennarthubbers/Desktop/processed-20250217_151129.hdf5"  # Replace with your actual file path
-file_path = "filtered_data4.hdf5"  # Replace with your actual file path
+file_path = "filtered_data_nn_2.hdf5"  # Replace with your actual file path
 
 def dat_array(dir):
 	with h5py.File(file_path, "r") as f:
